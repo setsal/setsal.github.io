865402abfac9526770414b9e1804d6afb39032b6
setsal Lan <contact@setsal.dev>
2020-08-24 10:54:58 +0800

    blog is under maintenance

---
+++ precache-manifest.0c02cf92cf0e15bcb951edbda48b7f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd70e80bc73cdc5a22ff856ad64a7656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0dd5779e10fea4409e",
+    "url": "/static/css/2.457ff3b6.chunk.css"
+  },
+  {
+    "revision": "99458bfb4dbd28676340",
+    "url": "/static/css/main.b5421dbf.chunk.css"
+  },
+  {
+    "revision": "2d0dd5779e10fea4409e",
+    "url": "/static/js/2.374da0c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.374da0c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99458bfb4dbd28676340",
+    "url": "/static/js/main.de1da93e.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  }
+]);
